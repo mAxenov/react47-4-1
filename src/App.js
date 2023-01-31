5fa471ea229594c51108ce297bc66930ef91a116
Maxim Axenov <topcmc32@gmail.com>
2023-01-31 22:00:16 +0500

    rgb

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [inputColor, setInputColor] = useState('#34495e');
+  const [backgroundColor, setBackgroundColor] = useState('#34495e');
+  const [outColor, setOutColor] = useState('rgb(52,73,94)');
+
+  function convertColor(string) {
+    if (string.length === 7) {
+      const re = new RegExp('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$');
+      if (re.test(string)) {
+        const strRgb = `rgb(${parseInt(string.substring(1, 3), 16)},${parseInt(string.substring(3, 5), 16)},${parseInt(string.substring(5, 7), 16)})`
+        setOutColor(strRgb);
+        setBackgroundColor(strRgb);
+      } else {
+        setOutColor('Ошибка!');
+        setBackgroundColor('rgb(255,0,0)')
+      }
+    }
+  }
+
+  function handlerInputChange(e) {
+    const value = e.target.value;
+    setInputColor(value);
+    convertColor(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: backgroundColor }} className="App">
+      <input className="input-color" onChange={handlerInputChange} value={inputColor} type="text" name="name" maxlength="7" />
+      <div className="input-color out-rgb">{outColor}</div>
     </div>
   );
 }
